668bc1074e864cbc77bf2c58f8b1ec6e81922aeb
TheBrokenRail <connor24nolan@live.com>
2019-01-09 19:55:20 -0500

    Initial commit

---
+++ app/src/main/java/com/thebrokenrail/stacktracetest/MainActivity.java
@@ -0,0 +1,83 @@
+
+package com.thebrokenrail.stacktracetest;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatSpinner;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int iterations = 1000;
+
+                ArrayList<Long> traceResults = new ArrayList<>();
+                ArrayList<Long> noTraceResults = new ArrayList<>();
+                for (int i = 0; i < iterations; i++) {
+                    traceResults.add(testTrace());
+                    noTraceResults.add(testNoTrace());
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                String average = getString(R.string.trace_avg) + " " + average(traceResults) + "\n" + getString(R.string.no_trace_avg) + " " + average(noTraceResults);
+                String total = getString(R.string.trace_total) + " " + total(traceResults) + "\n" + getString(R.string.no_trace_total) + " " + total(noTraceResults);
+                builder.setMessage(getString(R.string.iterations) + " " + iterations + "\n\n" + average + "\n\n" + total);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+
+    public double average(ArrayList<Long> list) {
+        long total = 0;
+        for (int i = 0; i < list.size(); i++) {
+            total = total + list.get(i);
+        }
+        return ((double) total) / list.size();
+    }
+
+    public long total(ArrayList<Long> list) {
+        long total = 0;
+        for (int i = 0; i < list.size(); i++) {
+            total = total + list.get(i);
+        }
+        return total;
+    }
+
+    private long testTrace() {
+        AppCompatSpinner spinner = findViewById(R.id.trace);
+        ArrayAdapter<CharSequence> adapter = new TraceAdapter<>(this, android.R.layout.simple_spinner_item, Arrays.asList(getResources().getTextArray(R.array.planets_array)), spinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        long start = System.currentTimeMillis();
+        spinner.measure(spinner.getWidth(), spinner.getHeight());
+        return System.currentTimeMillis() - start;
+    }
+
+    private long testNoTrace() {
+        AppCompatSpinner spinner = findViewById(R.id.noTrace);
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Arrays.asList(getResources().getTextArray(R.array.planets_array)));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        long start = System.currentTimeMillis();
+        spinner.measure(spinner.getWidth(), spinner.getHeight());
+        return System.currentTimeMillis() - start;
+    }
+}
---
+++ app/src/main/java/com/thebrokenrail/stacktracetest/TraceAdapter.java
@@ -0,0 +1,46 @@
+package com.thebrokenrail.stacktracetest;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatSpinner;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+
+/**
+ * Common Code for Field DropDown Adapters
+ * This makes a Spinner's width, the width of the currently selected item, not a one size fits all.
+ */
+class TraceAdapter<T> extends ArrayAdapter<T> {
+    private AppCompatSpinner mSpinner;
+
+    public TraceAdapter(@NonNull Context context, int resource, @NonNull List<T> objects, @Nullable AppCompatSpinner mSpinner) {
+        super(context, resource, objects);
+        this.mSpinner = mSpinner;
+    }
+
+    public TraceAdapter(@NonNull Context context, int resource, @Nullable AppCompatSpinner mSpinner) {
+        super(context, resource);
+        this.mSpinner = mSpinner;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Generate Stack Trace
+        StringWriter stackTrace = new StringWriter();
+        PrintWriter printer = new PrintWriter(stackTrace);
+        new RuntimeException().printStackTrace(printer);
+        // Use Stack Trace To Check if Method is being Called By android.widget.Spinner.measureContentWidth (fixes #735)
+        if (mSpinner != null && stackTrace.toString().contains("at android.widget.Spinner.measureContentWidth")) {
+            return super.getView(mSpinner.getSelectedItemPosition(), convertView, parent);
+        } else {
+            return super.getView(position, convertView, parent);
+        }
+    }
+}
